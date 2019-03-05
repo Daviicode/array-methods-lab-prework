16ccc3e538d6671e1fb9db2b2c82f8f777df9b4a
David zhang <d.ydzdavid@yahoo.com>
2019-03-05 23:53:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,13 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort(array)
 end
 
 def using_reverse(array)
-
+  array.reverse(array)
 end
 
 def using_first(array)
@@ -15,9 +15,9 @@ def using_first(array)
 end
 
 def using_last(array)
-
+  array.last(array)
 end
 
 def using_size(array)
-
+  array.size(array)
 end
